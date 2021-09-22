55f6955e5be1255110f4b5c30a58b4723dcc7fec
luca <lucaimbalzano@gmail.com>
2021-09-22 18:25:53 +0200

    #Initial Commit

---
+++ src/main/java/com/imbalzano/luca/LucaApplication.java
@@ -0,0 +1,13 @@
+package com.imbalzano.luca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LucaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imbalzano/luca/LucaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imbalzano.luca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
