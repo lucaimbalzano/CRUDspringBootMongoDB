f2f7946d0eaea0dbd23498c3882789a1ee86ffa5
luca <lucaimbalzano@gmail.com>
2021-10-08 15:47:54 +0200

    ## Swagger , App.yml

--- src/main/java/com/imbalzano/luca/repository/IUserRepository.java
+++ src/main/java/com/imbalzano/luca/repository/IUserRepository.java
@@ -3,10 +3,11 @@ package com.imbalzano.luca.repository;
 import com.imbalzano.luca.service.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface IUserRepository extends MongoRepository<User, Integer> {
 
 }
